4573997c76456d7f83f4bcf0c66a799589335dd7
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-07-27 22:58:00 +0800

    initialize the project

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+    return <h1>開發環境設置成功！</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({ 
+            template: './src/index.html' 
+        }),
+    ],
+
+    devServer: {
+        static: './dist',
+        https: false,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-react"],
+                    }
+                }
+            }
+        ],
+    }
+};
