676be99bcbd74e8bd73bc6f19b1febfc703ad7d8
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-09-18 23:30:01 +0800

    基本路由設定

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Bookmark from "./pages/02-Bookmark/Bookmark";
 import Overview from "./pages/10-Overview/Overview";
 import Features from "./pages/11-Features/Features";
 import Utilization from "./pages/12-Utilization/Utilization";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <>
-            {/* <Home /> */}
-            {/* <History /> */}
-            {/* <Bookmark /> */}
-            <Overview />
-            {/* <Features /> */}
-            {/* <Utilization /> */}
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/bookmark" element={<Bookmark />} />
+                <Route path="/overview/:spicesName" element={<Overview />} />
+                {/* 確認是否需要將 features 與 utilization 放到 overview 底下 */}
+                <Route path="/features/:spicesName" element={<Features />} />
+                <Route path="/utilization/:spicesName" element={<Utilization />} />
+                {/* 木 */}
+                <Route path="*" element={<h1>連線失敗，請回到首頁重新執行動作。</h1>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
