169eac35cf0c00aaea59f5aa4560e6cb7d52c34b
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-09-19 17:00:47 +0800

    處理基礎網址導向

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,36 +4,46 @@ import IconButton from '../IconButton/IconButton'
 import logoIcon from '../../images/logo.svg'
 import RecentIcon from '../../images/icon-recent.svg'
 import BookmarkIcon from '../../images/icon-bookmark.svg'
+import { Link } from "react-router-dom";
+import Home from "../../pages/00-Home/Home";
+import History from "../../pages/01-History/Histoy";
+import Bookmark from "../../pages/02-Bookmark/Bookmark";
 
 function Nav() {
     return (
         <nav className={Styles.nav}>
             <div className={Styles.navBlock}>
                 <IconButton>
-                    <img 
-                        src={logoIcon} 
-                        alt=""
-                        className={Styles.logoIcon}
-                    />
+                    <Link to="/" element={<Home />}>
+                        <img 
+                            src={logoIcon} 
+                            alt=""
+                            className={Styles.logoIcon}
+                        />
+                    </Link>
                 </IconButton>
             </div>
             <div className={Styles.navBlock}>
                 <IconButton>
-                    <img 
-                        src={RecentIcon} 
-                        alt=""
-                        className={Styles.recentIcon}
-                    />
+                    <Link to="/history" element={<History />}>
+                        <img 
+                            src={RecentIcon} 
+                            alt=""
+                            className={Styles.recentIcon}
+                        />
+                    </Link>
                 </IconButton>
                 <p>已搜尋</p>
             </div>
             <div className={Styles.navBlock}>
                 <IconButton>
-                    <img 
-                        src={BookmarkIcon} 
-                        alt=""
-                        className={Styles.bookmarkIcon}
-                    />
+                    <Link to="/bookmark" element={<Bookmark />}>
+                        <img 
+                            src={BookmarkIcon} 
+                            alt=""
+                            className={Styles.bookmarkIcon}
+                        />
+                    </Link>
                 </IconButton>
                 <p>已儲存</p>
             </div>
