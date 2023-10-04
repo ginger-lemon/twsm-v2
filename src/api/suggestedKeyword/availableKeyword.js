66c63eb5ceb43f054366a42015cd62c6ec6ba952
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-10-04 17:16:59 +0800

    re-adjust mock api data (JSON)

---
+++ src/api/suggestedKeyword/availableKeyword.js
@@ -0,0 +1,86 @@
+[
+    {
+        "key": "Litsea cubeba",
+        "name": "山胡椒/馬告",
+        "scientificname": "Litsea cubeba"
+    }, {
+        "key": "Zanthoxylum ailanthoides",
+        "name": "紅刺蔥",
+        "scientificname": "Zanthoxylum ailanthoides"
+    }, {
+        "key": "Cinnamomum osmophloeum",
+        "name": "土肉桂",
+        "scientificname": "Cinnamomum osmophloeum"
+    }, {
+        "key": "Aralia cordata",
+        "name": "食用土當歸",
+        "scientificname": "Aralia cordata"
+    }, {
+        "key": "Rhus javanica",
+        "name": "羅氏鹽膚木",
+        "scientificname": "Rhus javanica"
+    }, {
+        "key": "Perilla frutescens",
+        "name": "紫蘇",
+        "scientificname": "Perilla frutescens"
+    }, {
+        "key": "Alpinia zerumbet",
+        "name": "臺灣月桃",
+        "scientificname": "Alpinia zerumbet"
+    },
+    {
+        "key": "Alpinia flabellata",
+        "name": "呂宋月桃",
+        "scientificname": "Alpinia flabellata"
+    },
+    {
+        "key": "Alpinia uraiensis",
+        "name": "烏來月桃",
+        "scientificname": "Alpinia uraiensis"
+    },
+    {
+        "key": "Alpinia intermedia",
+        "name": "山月桃",
+        "scientificname": "Alpinia intermedia"
+    },
+    {
+        "key": "Machilus kusanoi",
+        "name": "大葉楠",
+        "scientificname": "Machilus kusanoi"
+    },
+    {
+        "key": "Hedychium coronarium",
+        "name": "野薑花",
+        "scientificname": "Hedychium coronarium"
+    },
+    {
+        "key": "Cryptotaenia japonica",
+        "name": "鴨兒芹",
+        "scientificname": "Cryptotaenia japonica"
+    },
+    {
+        "key": "Leonurus japonicus",
+        "name": "益母草",
+        "scientificname": "Leonurus japonicus"
+    },
+    {
+        "key": "Cardamine flexuosa",
+        "name": "焊菜",
+        "scientificname": "Cardamine flexuosa"
+    },
+    {
+        "key": "Oenanthe javanica",
+        "name": "水芹菜",
+        "scientificname": "Oenanthe javanica"
+    },
+    {
+        "key": "Houttuynia cordata",
+        "name": "魚腥草",
+        "scientificname": "Houttuynia cordata"
+    },
+    {
+        "key": "Artemisia indica",
+        "name": "艾草",
+        "scientificname": "Artemisia indica"
+    },
+]
