97dd176f95f7e0d5c584eac7551663cb4ad281f4
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-10-16 17:37:33 +0800

    deploy by firebase hosting, some config

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Bookmark from "./pages/Bookmark/Bookmark";
 import Results from "./pages/Results/Results";
 import Flavor from "./pages/Flavor/Flavor";
 import Utilization from "./pages/Utilization/Utilization";
-import { HashRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 const App = () => {
     const { id } = useParams()
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/history" element={<History />} />
@@ -22,7 +22,7 @@ const App = () => {
                 <Route path="/utilization/:id" element={<Utilization />} />
                 <Route path="*" element={<h1>連線失敗，請回到首頁重新執行動作。</h1>}/>
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
